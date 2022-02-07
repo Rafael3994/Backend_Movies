556368258da3bfdd3f3974dec6f012871eaade6d
Rafael3994 <rafael3994@gmail.com>
2022-02-07 23:33:56 +0100

    controllers

--- routes/peliculasAPI.js
+++ routes/peliculasAPI.js
@@ -1,38 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const axios = require('axios');
 
-
+const PeliculasApiController = require('../controllers/PeliculasApiController');
 const auth = require('../middleware/auth'); 
 
 // Endpoint todas las peliculas
 // GET: peliculas
-router.get('/', auth, async function(req, res, next) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES}`);
-    films = response.data.results;
-    res.json(films);
-});
+router.get('/', auth,  PeliculasApiController.getAllMovies);
 
 
 // Endpoint busqueda titulo
 // GET: peliculas/titulo/:titulo
-router.get('/titulo/:titulo', auth, async function(req, res, next) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES}`);
-    films = response.data.results;
-    filmsList = films.filter((film) =>
-        film.title.toLowerCase().indexOf(req.params.titulo.toLowerCase()) !== -1
-    );
-    res.json(filmsList);
-});
+router.get('/titulo/:titulo', auth, PeliculasApiController.getByTittle);
 
 // Endpoint busqueda id
 // GET: peliculas/id/:id
-router.get('/id/:id', auth, async function(req, res, next) {
-    const idFilm = req.params.id;
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${idFilm}?api_key=cea68b520beecac6718820e4ac576c3a`);
-    film = response.data;
-    res.json(film);
-});
+router.get('/id/:id', auth, PeliculasApiController.getById);
 
 
 module.exports = router;
